421c423a639b9ac5721c92b3a4b08b65465a6df4
Peng Zhang <pengzhang40@gmail.com>
2018-07-23 18:29:04 -0700

    adding project 2: pick and place

---
+++ project_2_robotic_arm/build/RoboND-Kinematics-Project/kuka_arm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/peng/RoboND/Term_1/project_2_robotic_arm/src/RoboND-Kinematics-Project/kuka_arm
+# Build directory: /home/peng/RoboND/Term_1/project_2_robotic_arm/build/RoboND-Kinematics-Project/kuka_arm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ project_2_robotic_arm/build/simple_arm/CMakeFiles/simple_arm_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_arm_generate_messages_lisp"
+  "/home/peng/RoboND/Term_1/project_2_robotic_arm/devel/share/common-lisp/ros/simple_arm/srv/GoToPosition.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/simple_arm_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
